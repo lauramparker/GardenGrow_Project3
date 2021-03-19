a0e6d3bcd6c0063fe4f4aa10970fae6913d9f33c
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-18 20:30:51 -0400

    add inital components

---
+++ client/src/pages/Landing Page/Landing.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Container from "../components/Container";
+import Header from "./components/Header";
+import Row from "./components/Row";
+import Col from "./components/Col";
+
+function Landing() {
+  return (
+    <div>
+      <Header />
+      <Container />
+      <Row />
+      <Col />
+
+    </div>
+  );
+}
+
+export default Landing;
+
+// Input group: 
+    // -Fields : Name / L x W / Date range 
