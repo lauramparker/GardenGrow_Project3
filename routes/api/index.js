29f52e6dc80439f0def8df9c7dc3c99cc1b42329
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-21 02:13:58 -0400

    include plant and garden routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const plantRoutes = require("./plant");
+const gardenRoutes = require("./garden");
+
+router.use("/plants", plantRoutes);
+router.use("/gardens", gardenRoutes);
 
 module.exports = router;
