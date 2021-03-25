174f35447d2136ee8a6da443d511853b7d8e3c5d
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-25 12:57:09 -0400

    add HashRouter/exact path on links/condensed navbars

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
+
 import { Container } from "reactstrap";
 
 import Loading from "./components/Loading";
 import NavBar from "./components/NavBar";
+// import Header from "./components/Header"
 import Footer from "./components/Footer";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
-import ExternalApi from "../src/views/ExternalApi";
+// import ExternalApi from "../src/views/ExternalApi";
 import { useAuth0 } from "@auth0/auth0-react";
 import history from "./utils/history";
 
 import Landing from "./pages/landing-page/Landing.js";
 import MyGarden from "./pages/my-gardens/MyGarden.js";
-import GardenEdit from "./pages/garden-edit/GardenEdit";
-import Garden from "./pages/Garden/garden";
+// import GardenEdit from "./pages/garden-edit/GardenEdit";
+// import Garden from "./pages/Garden/garden";
+
 
 // styles
 import "./App.css";
@@ -35,23 +38,22 @@ const App = () => {
   }
 
   return (
-    <Router history={history}>
+    <HashRouter history={history}>
       <div id="app" className="d-flex flex-column h-100">
         <NavBar />
+        {/* <Header /> */}
         <Container className="flex-grow-1 mt-5">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/external-api" component={ExternalApi} /> */}
-            <Route path="/landing-page" component={Landing} />
-            <Route path="/MyGardens" component={MyGarden} />
-            <Route path="/GardenEdit" component={GardenEdit} />
-            <Route path="/Garden" component={Garden} />
-          </Switch>
+            <Route exact path="/" exact component={Home} />
+            <Route exact path="/profile" component={Profile} />
+            {/* <Route exact path="/external-api" component={ExternalApi} /> */}
+            <Route exact path="/landing-page" component={Landing} />
+            <Route exact path="/MyGardens" component={MyGarden} />
+            {/* <Route exact path="/GardenEdit" component={GardenEdit} />
+            <Route exact path="/Garden" component={Garden} /> */}
         </Container>
         <Footer />
       </div>
-    </Router>
+    </HashRouter>
   );
 };
 
