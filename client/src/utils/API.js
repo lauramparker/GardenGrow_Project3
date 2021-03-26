7171d1b1135ec74f7f30c90308c29d0c044a1ce7
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-26 11:17:16 -0400

    work on form routes / add getOnePlant route

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,32 +4,37 @@ import axios from "axios";
 export default {
   // Gets all plants in db
   getPlants: function() {
-    return axios.get("http://localhost:3001/api/plants"); //added to fix CORS access error //remove when deploying
+    return axios.get("/api/plants"); //added to fix CORS access error //remove when deploying
   },
 
   // Get all gardens for specific user id ?????
   getGardens: function(user_id) {
-    return axios.get("http://localhost:3001/api/gardens/" + user_id);
+    return axios.get("/api/gardens/" + user_id);
   },
 
 
 // Get a Garden in the database with a specific id
   getOneGarden: function(id) {
-      return axios.get("http://localhost:3001/api/gardens/" + id);
+      return axios.get("/api/gardens/" + id);
+    },
+
+    // Get a plant with a specific id
+    getOnePlant: function(id) {
+      return axios.get("api/plants/" + id)
     },
 
   // Create (Saves) a NEW Garden to the database with a specific id
-  saveGarden: function(id) {
-    return axios.post("http://localhost:3001/api/gardens/" + id);
+  saveGarden: function(garden) {
+    return axios.post("/api/gardens", garden );
   },
 
    // Updates a Garden with array of plots as plants are added
-   updateGarden: function(id) {
-    return axios.put("http://localhost:3001/api/gardens/" + id);
+   updateGarden: function(id, data) {
+    return axios.put("/api/gardens/" + id, data);
   },
 
   deleteGarden: function(id) {
-    return axios.delete("http://localhost:3001/api/gardens/" + id);
+    return axios.delete("/api/gardens/" + id);
   },
 
   // Gets Plot Cards for a specific garden
