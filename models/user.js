282b3b905e5622253fc87b4f6a4143f0441bef5e
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-26 12:27:55 -0400

    export gardenSchema

--- models/user.js
+++ models/user.js
@@ -5,21 +5,21 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  gardens: {
-    type: Array,
-    of: gardenSchema
-  },
-  image: String
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	gardens: {
+		type: Array,
+		of: gardenSchema
+	},
+	image: String
 });
 
 const User = mongoose.model("User", userSchema);
