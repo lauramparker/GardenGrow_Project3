6a288036ec369fb2a7b65578e119f72bc2d6365c
Laura Parker <lauramparker78@gmail.com>
2021-03-26 13:59:36 -0400

    Resolving conflict

--- client/src/pages/garden-edit/GardenEdit.js
+++ client/src/pages/garden-edit/GardenEdit.js
@@ -4,18 +4,13 @@ import Footer from "../../components/Footer";
 
 
 
-//create a new array of the selected plants from the (all) plants list.
-//<List> component sends only the selected plants as plants to create the list/table component
-//in Garden component, plants is equal to all plants on the planning garden page
-// const selectedPlants = props.plants.filter(plant => plant.selected);
-
 function GardenEdit() {
   return (
     <div>
         <Container />
             <Row />
             <Col />
-                {/* <List plants={selectedPlants} /> */}
+              
       <Footer />
 
     </div>
