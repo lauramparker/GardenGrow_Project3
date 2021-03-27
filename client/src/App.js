749401eb8a661ffbb43d3e1ed4c5ac549efa9b44
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-27 09:38:14 -0400

    update to BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import { Container } from "reactstrap";
+
 import Loading from "./components/Loading";
 import NavBar from "./components/NavBar";
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
+// import ExternalApi from "../src/views/ExternalApi";
 import { useAuth0 } from "@auth0/auth0-react";
+
 import Landing from "./pages/landing-page/Landing.js";
 import MyGarden from "./pages/my-gardens/MyGarden.js";
 import GardenEdit from "./pages/garden-edit/GardenEdit";
 import Garden from "./pages/garden/Garden";
+
 
 // styles
 import "./App.css";
@@ -31,9 +36,10 @@ const App = () => {
   }
 
   return (
-    <HashRouter>
+    <Router>
       <div id="app" className="d-flex flex-column h-100">
         <NavBar />
+        <Switch>
         <Container className="flex-grow-1 mt-5">
             <Route exact path="/" component={Home} />
             <Route exact path="/profile" component={Profile} />
@@ -43,8 +49,9 @@ const App = () => {
             <Route exact path="/garden" component={Garden} />
         </Container>
         <Footer />
+        </Switch>
       </div>
-    </HashRouter>
+    </Router>
   );
 };
 
