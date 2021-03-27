4b6dcc05525bfbfd46b9697155363bae08349615
ehui eric <ehui_eric@yahoo.fr>
2021-03-27 03:02:21 -0400

    add hero image

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
-
+import GardenImage from "../assets/GardenGrow.jpeg";
+import "./home.css";
 
 const Home = () => (
 	<Fragment>
-		<p>Hello</p>
+		<img className="hero-image" src={GardenImage} alt="hero" />
 	</Fragment>
 );
 
