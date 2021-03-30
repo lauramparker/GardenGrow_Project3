9203a2aaf43dae88a1d5fbe2934e0131ca7f1e43
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-30 15:12:06 -0400

    change class to className, add a key to mapped elements in table, formatting

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -6,46 +6,46 @@ import "./style.css";
 function Table(props) {
 
     return (
-    <div>
-      <div className = "table-wrapper-scroll-y scrollbar">
-        <table className="table table-bordered table-hover" id="plantList">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Spacing</th>
-                <th>Harvest</th>
-               
-            </tr>
-        </thead>
-        <tbody>
-            {props.plants.map(plant => { 
-                return (
-                <tr>
-                    <td>
-                        <div class="form-check" id={plant._id}>
-                            <input 
-                                className="form-check-input"
-                                type="checkbox" 
-                                value={plant.Name} 
-                                onChange={props.handleSelectChange}
-                                > 
-                            </input>      
-                            <label className="form-check-label" htmlFor="flexCheckDefault">{plant.Name}</label>
-                        </div>
-                    </td> 
+        <div>
+            <div className="table-wrapper-scroll-y scrollbar">
+                <table className="table table-bordered table-hover" id="plantList">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Spacing</th>
+                            <th>Harvest</th>
 
-                    <td>{plant.spaceInstructions}</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.plants.map(plant => {
+                            return (
+                                <tr key={plant._id}>
+                                    <td>
+                                        <div className="form-check" id={plant._id}>
+                                            <input
+                                                className="form-check-input"
+                                                type="checkbox"
+                                                value={plant.Name}
+                                                onChange={props.handleSelectChange}
+                                            >
+                                            </input>
+                                            <label className="form-check-label" htmlFor="flexCheckDefault">{plant.Name}</label>
+                                        </div>
+                                    </td>
 
-                    <td>{plant.harvestInstructions}</td>
+                                    <td>{plant.spaceInstructions}</td>
 
-                </tr>
-                )
-            })
-            }
-        </tbody>
-        </table> 
-      </div>
-    </div>
+                                    <td>{plant.harvestInstructions}</td>
+
+                                </tr>
+                            )
+                        })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
 }
 
