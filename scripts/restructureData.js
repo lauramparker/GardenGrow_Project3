30fe00b7f9b4fdb2bc82e455b2d9bdd5c02b9cbc
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-30 21:45:48 -0400

    implement function to compute the possible number of plants in a 2x2 plot

--- scripts/restructureData.js
+++ scripts/restructureData.js
@@ -1,19 +1,26 @@
 const data = require("../data/plant-info.json");
+const PLOT_LENGTH = 24;
 
 const newData = JSON.parse(JSON.stringify(data), function (key, value) {
 	// rename all the name keys to lowercase name
-	if (key === "Name") {
-		this.name = value;
-		return;
-	}
+	// if (key === "Name") {
+	// 	this.name = value;
+	// 	return;
+	// }
 
 	// pull out the numbers from space instructions
 	if (key === "spaceInstructions") {
 		let numbers = value.match(/\d+/g);
 		if (numbers !== null) {
+			let numPlants = [];
 			numbers = numbers.map(Number);
+			numbers.forEach((number, index) => {
+				let plants = Math.floor((PLOT_LENGTH / number)**2);
+				if(plants >= 1) numPlants[index] = plants;
+				else numPlants[index] = 1;
+			});
 			this.inchesApart = numbers;
-			return;
+			this.numPlants = numPlants;
 		} else {
 			// do something
 		}
