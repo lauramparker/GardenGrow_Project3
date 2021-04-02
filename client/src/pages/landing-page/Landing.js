3e2ac82c8474440a268071e28ca5569b88f96ddd
Laura M. Parker <lauramparker78@gmail.com>
2021-04-01 17:47:56 -0700

    Update Landing.js
    putting AuthO here

--- client/src/pages/landing-page/Landing.js
+++ client/src/pages/landing-page/Landing.js
@@ -1,22 +1,60 @@
 import React from "react";
 import { Col, Row, Container } from "react-bootstrap";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import Loading from "../../components/Loading";
+import API from "../../utils/API";
 import Form from "../../components/Form";
 import "./Landing.css";
 
 
+
+
 function Landing() {
+
+  // const { isLoading, error } = useAuth0();
+
+  //     if (error) {
+  //       return <div>Oops... {error.message}</div>;
+  //     }
+
+  //     if (isLoading) {
+  //       return <Loading />;
+  //     }
+
+    const { user } = useAuth0();
+    const loggedInUser = localStorage.getItem("user") || "";
+
+  
+    console.log("user", user);
+    if (!loggedInUser) {
+      API.createUser({
+        lastName: user.family_name,
+        firstName: user.given_name,
+        userName: user.nickname,
+        email: user.email,
+        profilePicture: user.picture,
+      }).then((data) => {
+        console.log("user created", data);
+        localStorage.setItem("user", data._id);
+      });
+    }
+
+
+
   return (
     <div>
+      <Loading />;
       <Container >
-        <Form />
+
+          <Form></Form>
+
         <Row />
         <Col />
       </ Container>
     </div>
   );
-
 }
 
-export default Landing;
-
-// new garden should append here in list format. 
+export default withAuthenticationRequired(Landing, {
+  onRedirecting: () => <Loading />,
+});
