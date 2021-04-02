2a46abf97b3a4ae0ea2db5aa4cad177d87405923
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-04-01 21:00:29 -0400

    fixed the footer????? i think

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./style.css";
 
 const Footer = () =>  {
   return (
     <div className="container my-5">
 
-      <footer className="bg-light p-3 text-center" style={{ width: "100%", borderRadius: "5px", top: "0", bottom: "0" }}>
+      <footer className="bg-light p-3 text-center" style={{}}>
         <div className="logo">
           <span className="row d-flex justify-content-center">
             Garden Grow</span>
