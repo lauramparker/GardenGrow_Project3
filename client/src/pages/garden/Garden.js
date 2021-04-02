d84cddd1e70538058fc1d83bad6d6080709f13b0
Laura M. Parker <lauramparker78@gmail.com>
2021-04-01 17:47:05 -0700

    Update Garden.js
    merging state into develop

--- client/src/pages/garden/Garden.js
+++ client/src/pages/garden/Garden.js
@@ -1,115 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { GardenContext } from "../../Providers/GardenProvider";
 import { Col, Row, Container } from "react-bootstrap";
 import API from "../../utils/API";
 import CardContainer from "../../components/CardContainer";
 import Loading from "../../components/Loading";
 import Table from "../../components/Table";
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
-// import { useHistory } from "react-router-dom"; - for onsubmit
 import "./Garden.css";
-// import SearchForm from "../../components/SearchForm";
 
-function Garden() {
 
-  // let history = useHistory(); - for onsubmit
+
+function Garden() {   //{children}??
+
   //setting state for plants table to load plants in List table
-  const [plants, setPlants] = useState([]); //must be array for map to work (array of objects)
-  const { user } = useAuth0();
-  const [listObject, setListObject] = useState({
-    //set as object
-    id: "",
-    name: "",
-    spacing: "",
-    harvest: "",
-    image: "",
-  });
 
-  //garden , set Garden updated in Form
-  //we can separate garden into the form property needs and the garden page data needs
-  const [garden, setGarden] = useState({
-    id:"",
-    gardenName: " ",
-    length: "",
-    width: "",
-    garden_data: [
-    ],
-  });
+  const [plants, setPlants] = useState([]);
+
+
+  const { garden, setGarden } = useContext(GardenContext);
+
 
   //Load all plants and set to plants when Garden page renders
   useEffect(() => {
     API.getPlants()
       .then((res) => setPlants(res.data))
-      .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }, []);
 
+
+
+ 
+  // useParams(); ///
   useEffect(() => {
-    API.getUser(user.email)
-    .then((res) => setGarden(res.data[0].gardens))
-    .then((res) => console.log(res))
+    API.getOneGarden() //coming from new Garden State after form submission (id) or (garden._id)
+    .then((res) => setGarden(res.data)) //setGarden??
     .catch((err) => console.log(err));
   }, []);
 
 
-  // useEffect(() => {
-  //   console.log('garden', garden)
-  //   API.getOneGarden(garden.id)
-  //   .then((res) => setGarden(res.data))
-  //   .then((res) => console.log(res))
-  //   .catch((err) => console.log(err));
-  // }, []);
 
-
-//NEW VERSION
-    // useEffect((id) => {
-    //     API.updateGarden(id)
-    //     .then(res =>setGarden(res.data));
-    //  }, [garden.garden_data]);
-
-    //OLD VERSION
-//runs when garden container renders (like component did mount)
-    // useEffect((garden) => {
-    //    API.updateGarden({
-    //         method: 'PUT',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify({garden})
-    //     })
-    //         .then(res => res.json())
-    //         .then(data =>setGarden(data));
-    // }, []);
-
-
-    
-
-//When user selects plant from plant list, update component state 
-
-    function handleSelectChange(event)  {
-        const value = event.currentTarget.value
-        console.log(value);
-        setListObject({ ...listObject, name: value });
-            console.log(listObject);
-            addGardenData(listObject);
-    };
-
-
-//Adds selected plant data to garden_data.  
-//Updated garden state passes to CardContainer (data) and re-renders the cards
-    function addGardenData(listObject) {
-        setGarden(prevGarden => ({
-            garden_data: [...prevGarden.garden_data, {listObject}]  
-        }))
-        console.log(garden.garden_data);
-    };
-
-
-
-  //when the user saves their garden, need to reroute to MyGardens
+  
   function handleGardenSubmit(event) { 
     event.preventDefault();
-    alert("You planned your Garden! Want to start another?")
-    // history.push("/MyGarden"); -might need
-  }
-  console.log('gardens', garden);
+    useHistory.push("/MyGarden");  //reroute to MyGarden page
+  };
+
 
   return (
     <div>
@@ -126,7 +61,6 @@ function Garden() {
 
             <Table
               plants={plants}
-              handleSelectChange={handleSelectChange}
             ></Table>
           </Col>
         </Row>
@@ -142,7 +76,4 @@ function Garden() {
   );
 }
 
-export default  withAuthenticationRequired(Garden, {
-  onRedirecting: () => <Loading />,
-});
-
+export default Garden;
