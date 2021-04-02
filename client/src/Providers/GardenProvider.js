a3eb1bd82db369e19fe7f3e635a41dddfc41570c
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-04-01 21:36:16 -0400

    add default to switch, formatting

--- client/src/Providers/GardenProvider.js
+++ client/src/Providers/GardenProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState,} from 'react';
+import React, { createContext, useState, } from 'react';
 import { useHistory } from "react-router-dom";
 import API from '../utils/API';
 
@@ -9,22 +9,22 @@ export const GardenContext = createContext();
 const GardenProvider = ({ children }) => {
 
 
-    const [garden, setGarden] = useState({
-        // id:"", //don't need?? _id automatic with mongo
-        date: "",
-        gardenName: "",
-        length: "",
-        width: "",
-        garden_data: [
-        ], //attached plant data
-    });
+  const [garden, setGarden] = useState({
+    // id:"", //don't need?? _id automatic with mongo
+    date: "",
+    gardenName: "",
+    length: "",
+    width: "",
+    garden_data: [
+    ], //attached plant data
+  });
 
-  
+
   const handleChange = (e) => {
     const { name, value, type } = e.target;
-    console.log({ name, value, type});
+    console.log({ name, value, type });
 
-    switch(type) {
+    switch (type) {
       case 'text':
       case 'select-one':
         return setGarden({
@@ -32,70 +32,72 @@ const GardenProvider = ({ children }) => {
           [name]: value
         });
 
-      case 'submit': 
+      case 'submit':
         return setGarden({
           ...garden,
           gardenName: "",
           length: "",
           width: ""
-        //   date: ""
+          //   date: ""
         });
+      default: break;
     }
-};
+  };
 
-//CREATE POST A new Garden w Name, Length, Width, Date
-const handleSubmit = (e) => {
+  //CREATE POST A new Garden w Name, Length, Width, Date
+  const handleSubmit = (e) => {
     e.preventDefault();
-      API.saveGarden({
-        gardenName: garden.gardenName,
-        length: garden.length,
-        width: garden.width,
-        // date: dateRange.startDate //sending dateRange from local state
-      }).then(res => {setGarden({ //setGarden?
+    API.saveGarden({
+      gardenName: garden.gardenName,
+      length: garden.length,
+      width: garden.width,
+      // date: dateRange.startDate //sending dateRange from local state
+    }).then(res => {
+      setGarden({ //setGarden?
         gardenName: res.data.gardenName,
         length: res.data.length,
         width: res.data.length,
         // date: garden.date,
         // id: res.data._id //need to get Garden ID
       })
-        useHistory.push("/Garden" + garden._id)   //erring out
-      }).catch((err) => console.log(err));
+      useHistory.push("/Garden" + garden._id)   //erring out
+    }).catch((err) => console.log(err));
   };
 
 
-//When user selects plant from plant list, update component state 
-  const handleSelect = (e) =>  {
-    const value  = e.currentTarget.value  //need to destructure to get all plant info
-    
-        return setGarden(prevGarden => ({
-          garden_data: [...prevGarden.garden_data, (value)]  ///  garden_data: [...prevGarden.garden_data, {plant}]  ///
-      }))
+  //When user selects plant from plant list, update component state 
+  const handleSelect = (e) => {
+    const value = e.currentTarget.value  //need to destructure to get all plant info
+
+    return setGarden(prevGarden => ({
+      garden_data: [...prevGarden.garden_data, (value)]  ///  garden_data: [...prevGarden.garden_data, {plant}]  ///
+    }))
       .catch((err) => console.log(err));
   };
 
 
   const handleSave = (e) => { //where does PUT/Update route go? also see garden.js (as handle submit, no routing)
-      API.updateGarden()  //(id)
-      .then(res =>setGarden(res.data))
+    API.updateGarden()  //(id)
+      .then(res => setGarden(res.data))
       .catch((err) => console.log(err));
   };
 
 
-    return (
-        <>
-        <GardenContext.Provider
-            value={{
-            garden,
-            handleChange,
-            handleSelect,
-            handleSave,
-            handleSubmit
-            }}
-        >
-            {children}
-        </GardenContext.Provider>
-        </>
-    )
+  return (
+    <>
+      <GardenContext.Provider
+        value={{
+          garden,
+          handleChange,
+          handleSelect,
+          handleSave,
+          handleSubmit
+        }}
+      >
+        {children}
+      </GardenContext.Provider>
+    </>
+  )
 };
 
 export default GardenProvider;
