fc71a97a5672456cbd1d687b92510ddfd7d87b51
Laura Parker <lauramparker78@gmail.com>
2021-04-03 01:17:50 -0400

    Moving handleSave / update Garden to Garden Provider

--- client/src/pages/garden/Garden.js
+++ client/src/pages/garden/Garden.js
@@ -4,7 +4,6 @@ import { GardenContext } from "../../Providers/GardenProvider";
 import { Col, Row, Container } from "react-bootstrap";
 import API from "../../utils/API";
 import CardContainer from "../../components/CardContainer";
-import Loading from "../../components/Loading";
 import Table from "../../components/Table";
 import "./Garden.css";
 
@@ -16,8 +15,8 @@ function Garden() {   //{children}??
 
   const [plants, setPlants] = useState([]);
 
+  const { garden, handleSave } = useContext(GardenContext);
 
-  const { garden, setGarden } = useContext(GardenContext);
 
 
   //Load all plants and set to plants when Garden page renders
@@ -29,21 +28,21 @@ function Garden() {   //{children}??
 
 
 
- 
-  // useParams(); ///
+  // send user to api/garden/:id after Garden state updates
   useEffect(() => {
-    API.getOneGarden() //coming from new Garden State after form submission (id) or (garden._id)
-    .then((res) => setGarden(res.data)) //setGarden??
-    .catch((err) => console.log(err));
-  }, []);
+    const id = garden.id
+    // if (garden.id !==0) {
+    //   window.location.assign("/Garden/" + id);
+    // }
+  }, [garden.id]);
 
 
-
-  
-  function handleGardenSubmit(event) { 
-    event.preventDefault();
-    useHistory.push("/MyGarden");  //reroute to MyGarden page
-  };
+   
+//delete this....
+  // function handleGardenSubmit(event) { 
+  //   event.preventDefault();
+  //   useHistory.push("/MyGarden");  //reroute to MyGarden page
+  // };
 
 
   return (
@@ -66,7 +65,7 @@ function Garden() {   //{children}??
         </Row>
         <Row>
           <Col>
-            <button className="btn" id="saveBtn" onClick={handleGardenSubmit}>
+            <button className="btn" id="saveBtn" onClick={handleSave}>
               Save Garden
             </button>
           </Col>
