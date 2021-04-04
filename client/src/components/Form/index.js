d5c63fc01cfe4782a6df416385ef5320ce505b7c
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-04-04 13:07:56 -0400

    format

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -10,8 +10,6 @@ import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import "./style.css";
 
 const Form = ({ children }) => {
-
-
   // state for date range pickr // component needs as []
   // const [dateRange, setDateRange] = useState([
   //   {
@@ -21,19 +19,15 @@ const Form = ({ children }) => {
   //   }
   // ]);
 
+  // hooks
   const { user } = useAuth0();
 
-  // const history = useHistory();
- 
-  const { garden, setGarden } = useContext(GardenContext); 
-
+  const { garden, setGarden } = useContext(GardenContext);
 
   const [name, setName] = useState("");
   const [length, setLength] = useState("");
   const [width, setWidth] = useState("");
   const [gardenData, setGardenData] = useState(null)
-
-
 
   //CREATE POST A new Garden w Name, Length, Width, Date
   const handleSubmit = (e) => {
@@ -51,14 +45,14 @@ const Form = ({ children }) => {
       dateRangeMinimum: null,
       dateRangeMaximum: null,
     })
-  }; 
+  };
 
   useEffect(() => {
     if (gardenData) {
       API.saveGarden({
         userId: user.email,
         ...gardenData
-      }).then (res => {
+      }).then(res => {
         setGarden({
           gardenName: res.data.gardenName,
           length: res.data.length,
@@ -66,97 +60,94 @@ const Form = ({ children }) => {
           dateRangeMinimum: null,
           dateRangeMaximum: null,
           plots: [],
-          id: res.data._id 
+          id: res.data._id
         })
       }).catch((err) => console.log(err));
     } //end if 
   }, [gardenData]);
 
-
-
-
-      return (
-        <div className="form-container" style={{ height: "325px" }}>
-          <div>
-            <form
-              onSubmit={handleSubmit}
-              style={{
-                width: "750",
-                border: "2px solid",
-                borderColor: "#73AD21",
-                position: "absolute",
-                top: "50%",
-                left: "35%",
-                transform: "translate(-25%, -25%)",
-                borderRadius:"15px"
-              }}
-            >{children}
-              <div className="mt-4" style={{ textAlign: "center" }}>
-                <h3>Garden Parameters</h3>
-              </div>
-              <Container style={{ width: "550" }} className="mt-3 px-5">
-                <Row className="form-group">
-                  <Col size="6">
-                    <input
-                      onChange={(event) => setName(event.target.value)}
-                      className="form-control"
-                      type="select-one"
-                      placeholder="Add garden name..."
-                      name="gardenName"
-                      value= {name}
-                    />{children}
-                  </Col>
-                </Row>
-                <Row className="form-group">
-                  <Col size="6">
-                    <label>Length</label>
-                    <br></br>
-                    <select
-                      onChange={(event) => setLength(event.target.value)}
-                      className="form-control"
-                      type="select-one"
-                      name="length"
-                      value={length}  
-                    >{children}
-                      <option value="2">2</option>
-                      <option value="4">4</option>
-                      <option value="6">6</option>
-                      <option value="8">8</option>
-                      <option value="10">10</option>
-                    </select>
-                  </Col>
-                  <Col size="6">
-                    <label>Width</label>
-                    <br></br>
-                    <select
-                      onChange={(event) => setWidth(event.target.value) }
-                      className="form-control"
-                      type="text"
-                      name="width"
-                      value={width}
-                    >{children}
-                      <option value="2">2</option>
-                      <option value="4">4</option>
-                      <option value="6">6</option>
-                      <option value="8">8</option>
-                      <option value="10">10</option>
-                    </select>
-                  </Col>
-                </Row>
+  return (
+    <div className="form-container" style={{ height: "325px" }}>
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            width: "750",
+            border: "2px solid",
+            borderColor: "#73AD21",
+            position: "absolute",
+            top: "50%",
+            left: "35%",
+            transform: "translate(-25%, -25%)",
+            borderRadius: "15px"
+          }}
+        >{children}
+          <div className="mt-4" style={{ textAlign: "center" }}>
+            <h3>Garden Parameters</h3>
+          </div>
+          <Container style={{ width: "550" }} className="mt-3 px-5">
+            <Row className="form-group">
+              <Col size="6">
+                <input
+                  onChange={(event) => setName(event.target.value)}
+                  className="form-control"
+                  type="select-one"
+                  placeholder="Add garden name..."
+                  name="gardenName"
+                  value={name}
+                />{children}
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col size="6">
+                <label>Length</label>
                 <br></br>
-                <Row className="form-group">
-                  <button
-                    className="btn btn-success"
-                    type="submit"
-                  >{children}
+                <select
+                  onChange={(event) => setLength(event.target.value)}
+                  className="form-control"
+                  type="select-one"
+                  name="length"
+                  value={length}
+                >{children}
+                  <option value="2">2</option>
+                  <option value="4">4</option>
+                  <option value="6">6</option>
+                  <option value="8">8</option>
+                  <option value="10">10</option>
+                </select>
+              </Col>
+              <Col size="6">
+                <label>Width</label>
+                <br></br>
+                <select
+                  onChange={(event) => setWidth(event.target.value)}
+                  className="form-control"
+                  type="text"
+                  name="width"
+                  value={width}
+                >{children}
+                  <option value="2">2</option>
+                  <option value="4">4</option>
+                  <option value="6">6</option>
+                  <option value="8">8</option>
+                  <option value="10">10</option>
+                </select>
+              </Col>
+            </Row>
+            <br></br>
+            <Row className="form-group">
+              <button
+                className="btn btn-success"
+                type="submit"
+              >{children}
                     Submit
                   </button>
-                </Row>
-              </Container>
-            </form>
-          </div>
-        </div>
-      );
-    }
+            </Row>
+          </Container>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Form;
