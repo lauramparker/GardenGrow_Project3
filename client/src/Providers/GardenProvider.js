d9b4cc175ea02da6756e2478e67dd9295d40a124
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-04-04 00:55:14 -0400

    format

--- client/src/Providers/GardenProvider.js
+++ client/src/Providers/GardenProvider.js
@@ -4,28 +4,20 @@ import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from "../components/Loading";
 import API from '../utils/API';
 
-
-
 export const GardenContext = createContext();
 
-
 const GardenProvider = ({ children }) => {
-
   const { user } = useAuth0();
-
   const history = useHistory();
-
   const [garden, setGarden] = useState();
-
   const [plants, setPlants] = useState([]);
 
-   //Load all plants - don't pass plants to Garden.js
-   useEffect(() => {
+  //Load all plants - don't pass plants to Garden.js
+  useEffect(() => {
     API.getPlants()
       .then((res) => setPlants(res.data))
       .catch((err) => console.log(err));
   }, []);
-  
 
   useEffect(() => {
     if (garden && garden.id) {
@@ -33,35 +25,27 @@ const GardenProvider = ({ children }) => {
     }
   }, [garden])
 
-
-  
-  
-
-  const handleSave = (e) => { 
-    API.updateGarden(garden.id) 
+  const handleSave = (e) => {
+    API.updateGarden(garden.id)
       .then(res => setGarden(res.data))
       .catch((err) => console.log(err));
-      useHistory.push("/MyGarden");  //reroute to MyGardens after garden saved
+    useHistory.push("/MyGarden");  //reroute to MyGardens after garden saved
   };
 
-
   return (
-    <>
-      <GardenContext.Provider
-        value={{
-          garden,
-          setGarden,
-          plants,
-          setPlants,
-          handleSave,
-        }}
-      >
-        {children}
-      </GardenContext.Provider>
-    </>
+    <GardenContext.Provider
+      value={{
+        garden,
+        setGarden,
+        plants,
+        setPlants,
+        handleSave,
+      }}
+    >
+      {children}
+    </GardenContext.Provider>
   )
 };
-
 
 export default withAuthenticationRequired(GardenProvider, {
   onRedirecting: () => <Loading />,
