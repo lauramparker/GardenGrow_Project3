22a979cb38e949b7a969e7b0f9ce40cb9736f7a3
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-04-06 15:16:03 -0400

    update useEffect to only run once

--- client/src/pages/garden/Garden.js
+++ client/src/pages/garden/Garden.js
@@ -9,15 +9,15 @@ import "./Garden.css";
 import { useParams } from "react-router-dom";
 import Loading from "../../components/Loading"
 
-function Garden() { 
+function Garden() {
   const { garden, setGarden, plants, handleSave } = useContext(GardenContext)
-
   const { id } = useParams();
 
   // When user selects plant from plant list, update component state 
   const handleSelectChange = (e) => {
     const value = e.currentTarget.value;
     let selectedPlant = plants.filter(plant => plant.Name === value);
+
     selectedPlant = selectedPlant[0];
     console.log("You selected this plant: " + selectedPlant.Name);
 
@@ -27,10 +27,12 @@ function Garden() {
     }))
   };
 
-  useEffect(() => {
-    API.getOneGarden(id)
-      .then(res => setGarden(res.data))
-  }, [id, setGarden, garden]);
+useEffect(() => {
+  API.getOneGarden(id)
+  .then(res => setGarden(res.data))
+  .catch(err => console.error(err));
+}, [id, setGarden]);
+  
 
   if (!garden) return <Loading />;
   else {
@@ -52,7 +54,7 @@ function Garden() {
         </Row>
         <Row>
           <Col>
-            <button style={{backgroundColor:"#F2B199", color:"white"}} className="btn" id="saveBtn" onClick={handleSave}>
+            <button style={{ backgroundColor: "#F2B199", color: "white" }} className="btn" id="saveBtn" onClick={handleSave}>
               Save Garden
             </button>
           </Col>
