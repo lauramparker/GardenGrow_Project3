36c16763bc78883a0e3bc7dc83b918b2b4a694f5
ehui eric <ehui_eric@yahoo.fr>
2021-04-06 22:56:54 -0400

    install path

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -17,8 +18,8 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-app.get('*',(req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get("*",(req, res) => {
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 // Connect to the Mongo DB
